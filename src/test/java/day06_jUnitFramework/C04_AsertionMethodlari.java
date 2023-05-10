cac1448b366cc2c36d57b846aa2dd44fea64ff88
zsk <zsk199207@gmail.com>
2023-05-10 03:36:39 +0300

    day06_gun_sonu

--- src/test/java/day06_jUnitFramework/C04_AsertionMethodlari.java
+++ src/test/java/day06_jUnitFramework/C04_AsertionMethodlari.java
@@ -3,7 +3,7 @@ package day06_jUnitFramework;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class AsertionMethodlari {
+public class C04_AsertionMethodlari {
 
     @Test
     public void test01() {
